d54a174aca9f1e42563b28a7af59333456b16159
Administrador <Administrador@JUPTER>
2023-01-18 19:49:47 -0300

    Adicionar arquivos de projeto.

---
+++ appeufiz/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using appeufiz.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace appeufiz.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
